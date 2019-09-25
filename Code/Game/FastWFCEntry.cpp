63b307e1f1a0c1623c754b7118a5940a5e161c49
Pronay <pronay.y2k@gmail.com>
2019-09-24 23:35:43 -0500

    Made changes to ColorGuide, writing run debug messages to log file and adding new office template based on MagicOffice

--- Code/Game/FastWFCEntry.cpp
+++ Code/Game/FastWFCEntry.cpp
@@ -1,6 +1,7 @@
 #include "Engine/Commons/EngineCommon.hpp"
 #include "Engine/Core/Time.hpp"
 #include "Engine/Core/FileUtils.hpp"
+#include "Engine/Commons/LogSystem.hpp"
 #include "Engine/Core/WindowContext.hpp"
 #include "Engine/Math/RandomNumberGenerator.hpp"
 #include "Game/FastWFCEntry.hpp"
@@ -12,7 +13,7 @@
 WFCSettings_T gWFCSettings;
 
 //Read the overlapping WFC problem from the XML node
-void ReadOverlappingInstance(tinyxml2::XMLElement* node)
+void ReadOverlappingInstance(tinyxml2::XMLElement* node, int problemIndex)
 {
 	std::string name = ParseXmlAttribute(*node, "name", "");
 	uint N = ParseXmlAttribute(*node, "N", 3);
@@ -29,8 +30,12 @@ void ReadOverlappingInstance(tinyxml2::XMLElement* node)
 	uint height = ParseXmlAttribute(*node, "height", gWFCSettings.defaultHeight);
 
 	DebuggerPrintf("\n\n Started WFC for Overlapping problem %s", name.c_str());
+	g_LogSystem->Logf("WFC System", "\n\n Started WFC for Overlapping problem %s", name.c_str());
+	
 	float startTime = (float)GetCurrentTimeSeconds();
+	
 	DebuggerPrintf("\n Start Time: %f", startTime);
+	g_LogSystem->Logf("WFC System", "\n Start Time: %f", startTime);
 	
 	const std::string image_path = gWFCSettings.imageReadPath + name + ".png";
 	std::optional<Array2D<Color>> imageColorArray = ReadImage(image_path);
@@ -45,8 +50,15 @@ void ReadOverlappingInstance(tinyxml2::XMLElement* node)
 	//Write all the patterns to a patterns folder
 	std::string outFolderPath = gWFCSettings.imageOutPath + name;
 	outFolderPath += "/";
-
 	g_windowContext->CheckCreateDirectory(outFolderPath.c_str());
+
+	std::string outFolderKernelsPath = outFolderPath;
+	outFolderKernelsPath += "/Kernels/";
+	g_windowContext->CheckCreateDirectory(outFolderKernelsPath.c_str());
+
+	//Let's account for different problems with the same name
+	outFolderPath += "/Problem_" + std::to_string(problemIndex) + "_";
+	outFolderKernelsPath += "/Problem_" + std::to_string(problemIndex) + "_";
 
 	for (unsigned i = 0; i < numOutputImages; i++) 
 	{
@@ -62,24 +74,29 @@ void ReadOverlappingInstance(tinyxml2::XMLElement* node)
 
 				for (int patternIndex = 0; patternIndex < patterns.size(); patternIndex++)
 				{
-					WriteImageAsPNG(outFolderPath + "Run_" + std::to_string(i) + "_Kernel_" + std::to_string(patternIndex) + ".png", patterns[patternIndex]);
+					WriteImageAsPNG(outFolderKernelsPath + "Run_" + std::to_string(i) + "_Kernel_" + std::to_string(patternIndex) + ".png", patterns[patternIndex]);
 				}
 
 				WriteImageAsPNG(outFolderPath + name + "_" + std::to_string(i) + ".png", *success);
 				DebuggerPrintf("\n Finished solving problem %s", name.c_str());
+				g_LogSystem->Logf("WFC System", "\n Finished solving problem %s", name.c_str());
 				break;
 			}
 			else 
 			{
 				DebuggerPrintf("\n Failed to solve problem %s", name.c_str());
+				g_LogSystem->Logf("WFC System", "\n Failed to solve problem %s", name.c_str());
 			}
 		}
 	}
 
 	float endTime = (float)GetCurrentTimeSeconds();
 	DebuggerPrintf("\n End Time: %f", endTime);
+	g_LogSystem->Logf("WFC System", "\n End Time: %f", endTime);
+
 	float timeTaken = endTime - startTime;
 	DebuggerPrintf("\n Time take for problem: %f", timeTaken);
+	g_LogSystem->Logf("WFC System", "\n Time take for problem: %f", timeTaken);
 }
 
 void SetTimeStampedOutPath()
@@ -109,11 +126,15 @@ void ReadConfigFile(const std::string &config_path) noexcept
 	tinyxml2::XMLElement* root = meshDoc.RootElement();
 	tinyxml2::XMLElement* node = root->FirstChildElement("overlapping");
 
+	int problemIndex = 1;
+
 	while (node != nullptr)
 	{
-		ReadOverlappingInstance(node);
+		ReadOverlappingInstance(node, problemIndex);
 
 		node = node->NextSiblingElement("overlapping");
+
+		++problemIndex;
 	}
 }
 
--- Run/Data/Gameplay/ColorGuide.md
+++ Run/Data/Gameplay/ColorGuide.md
@@ -2,8 +2,10 @@
 
 #Color Code to interpretation#
 
-	-	WHITE (255,255,255): 	Empty
-	-	BLACK (0,0,0): 			Wall
-	-	PURE BLUE (0, 0, 255):	Glass Wall
-	-	PURE RED (255, 0, 0):	Wet Wall
+	-	WHITE (255,255,255): 			Empty
+	-	BLACK (0,0,0): 					Wall	
+	-	PURE BLUE (0, 0, 255):			Glass Wall
+	-	PURE RED (255, 0, 0):			Wet Wall
+	-	LIGHT BROWN (255, 128, 0): 		Tables
+	-	PURE YELLOW (255, 255, 0):		Chairs
 	-	
